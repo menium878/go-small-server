fe98711c314e4f5e6cc6106c49bfb95757a57ba4
unknown <meus13@gmail.com>
2022-11-24 01:27:57 +0100

    komentarze

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// obsługa form
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -18,6 +19,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Adres = %s\n", address)
 }
 
+// tutaj obsługa hello
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
