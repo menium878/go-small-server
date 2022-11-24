3ccd8e2e42955aa1b11291b619d595aa435294ab
unknown <meus13@gmail.com>
2022-11-24 01:43:04 +0100

    Łączenie branch

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Hello")
 }
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
